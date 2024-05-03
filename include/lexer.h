affe87ac063f83ba22c030e0d859edd4e95fc50e
saho-north <SahoKitahara@my.uopeople.edu>
2024-05-03 23:23:47 +0900

    updated enum types

--- include/lexer.h
+++ include/lexer.h
@@ -1,8 +1,40 @@
 /* lexer.h - 入力をトークンに分割する字句解析器の定義。 */
 #ifndef LEXER_H
 # define LEXER_H
-# include "struct.h"
 
-t_token	*tokenize(char *s);
+# include <string.h> // strchr, strncmp, strudup, strlen
+# include <stdlib.h> // malloc, free
+# include <stdbool.h> // bool
+
+typedef enum e_token_type
+{
+	TK_UNDEF_TOKEN = -1, // 未定義のトークンは通常存在しない値を使う
+	TK_PARSE_ERROR = -2, // 構文エラー
+	TK_EOF = '\0',       // EOF
+
+	// word token
+	TK_WORD = 'a',         // WORD 一般的な単語や文字列
+	TK_SQUOTED_STR = '\'', // シングルクオート文字列
+	TK_DQUOTED_STR = '\"', // ダブルクオート文字列
+
+	// meta and op token
+	TK_PIPE = '|',         // パイプ
+	TK_REDIR_IN = '<',     // リダイレクト入力
+	TK_REDIR_OUT = '>',    // リダイレクト出力
+	TK_HEREDOC = 'H',      // ヒアドキュメント、大文字で他と区別
+	TK_REDIR_APPEND = 'A', // リダイレクト追加、大文字で他と区別
+	TK_WHITESPACE = ' ',   // 空白
+	TK_TAB = '\t',         // タブ
+	TK_NEWLINE = '\n'      // 改行
+}						t_token_type;
+
+typedef struct s_token
+{
+	enum e_token_type	type;
+	char				*word;
+	struct s_token		*next;
+}						t_token;
+
+t_token					*lexer(char *s);
 
 #endif
