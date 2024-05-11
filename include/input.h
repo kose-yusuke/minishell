fa1a312379e5fe097a08512ad71d46f454f773c8
saho-north <SahoKitahara@my.uopeople.edu>
2024-05-11 15:16:57 +0900

    .

---
+++ include/input.h
@@ -0,0 +1,17 @@
+/**
+ * readline 関数のラッパー、履歴の初期化と更新、入力の処理と解析など
+ */
+#ifndef READLINE_H
+# define READLINE_H
+
+# include "input.h"
+# include "lexer.h"
+# include "minishell.h"
+# include "parser.h"
+# include "token.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+
+int	input(char **envp, t_mgr *mgr);
+
+#endif
