d3ec7f17a7d1c79c74549e2bda096e414b32be2b
kose-yusuke <yusuke188alc@icloud.com>
2024-05-17 17:57:45 +0900

    echoなど一通り実装(envが関わりそうなbuiltinと実装少し大変そうなcd以外)

--- include/builtins.h
+++ include/builtins.h
@@ -3,11 +3,27 @@
 # define BUILTINS_H
 
 # include "minishell.h"
+#include <string.h>
 /* builtin.c */
 bool	is_builtin(t_execcmd *ecmd);
 int		exec_builtin(t_execcmd *ecmd);
 
 /* builtin_exit.c */
 int builtin_exit(char **argv);
+/* builtin_export.c */
+int	builtin_export(char **argv);
+/* builtin_unset.c */
+int	builtin_unset(char **argv);
+/* builtin_pwd.c */
+int	builtin_pwd(char **argv);
+/* builtin_env.c */
+int	builtin_env(char **argv);
+/* builtin_echo.c */
+int	builtin_echo(char **argv);
+/* builtin_cd.c */
+int	builtin_cd(char **argv);
+
+/* utils */
+size_t	ft_strlen(const char *str);
 
 #endif
---
+++ src/builtin_echo.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 17:28:09 by koseki.yusu       #+#    #+#             */
+/*   Updated: 2024/05/17 17:35:53 by koseki.yusu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	builtin_echo(char **argv)
+{
+	bool	is_first_arg;
+	bool	echo_newline;
+	size_t	i;
+
+	i = 1;
+	echo_newline = true;
+    //オプション -n がある場合、最後に改行しない.
+	if (argv[1] && strncmp(argv[1], "-n", 2) == 0)
+	{
+		i++;
+		echo_newline = false;
+	}
+	is_first_arg = true;
+	while (argv[i])
+	{
+		if (!is_first_arg)
+			write(1, " ", 1);
+		is_first_arg = false;
+		write(1, argv[i], ft_strlen(argv[i]));
+		i++;
+	}
+	if (echo_newline)
+		write(1, "\n", 1);
+	return (0);
+}
