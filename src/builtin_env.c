ff41863ff0593631aa7944c9346a233eed1dad77
kose-yusuke <yusuke188alc@icloud.com>
2024-05-22 15:51:37 +0900

    builtin-export,envの環境変数処理追加

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:20:10 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/05/17 17:23:44 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/05/22 15:43:29 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ int	builtin_env(char **argv)
         return (1);
 	//オプションの確認
     // 全てのenvを表示
+    while ()
+    {
+
+    }
+    printf("_=/usr/bin/env");
 	return (0);
 }
--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:54:15 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/05/17 17:09:27 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/05/22 15:50:54 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,36 @@ void print_all_env()
     // 全ての環境変数を出力する.
 }
 
+int set_key_value(char *string, char **key, char **value)
+{
+    char *key_end;
+
+    key_end = ft_strchr(string, '=');
+    if (key_end == NULL)
+    {
+        *key = string;
+        *value = NULL;
+        return (-1);
+    }
+    else
+    {
+        *key = ft_strndup(string, key_end - string);
+        *value = ft_strdup(key_end + 1);
+    }
+    if (*key != NULL && *value != NULL)
+        insert(t_hash_table *table, *key, *value);
+    else
+        return (-1);
+    return (1);
+}
+
 // 返り値 : 正常が0, エラーは1
 int	builtin_export(char **argv)
 {
     int i;
+    int status;
+    char **key;
+    char **value;
     
     if (argv[1] == NULL)
     {
@@ -28,14 +54,16 @@ int	builtin_export(char **argv)
         return (0);
     }
     i = 1;
+    status = 0;
     while(argv[i])
     {
-        // if (環境変数のハッシュテーブルに追加する.)
+        if((set_key_value(argv[i]), &key, &value) < 0)
         {
             // エラーの場合
-            return (1);
+            builtin_error("export", argv[i], "not a valid identifier");
+            status = 1;
         }
         i++; //代入される変数の分だけ繰り返す
     }
-    return (0);
+    return (status);
 }
