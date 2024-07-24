63ccf9bc6ad1fd7a819d16262cc5c0e6aaf03bd0
skitheom <SahoKitahara@my.uopeople.edu>
2024-07-25 02:45:46 +0900

    コメントの追加とrefactoring

--- src/expander.c
+++ src/expander.c
@@ -2,72 +2,6 @@
 #include "expander.h"
 #include "minishell.h"
 
-void	expand_env_var(t_word *word_list, t_mgr *mgr)
-{
-	char	*dollar_ptr;
-	char	*env_key;
-	char	*env_tail;
-	char	*value;
-	size_t	len;
-	char	*new_word;
-
-	dollar_ptr = strchr(word_list->token->word, '$');
-	if (!dollar_ptr || *(dollar_ptr + 1) == '\0')
-		return ;
-	env_tail = dollar_ptr + 1;
-	while (*env_tail && !strchr(IFS, *env_tail))
-		env_tail++;
-	env_key = strndup(dollar_ptr + 1, env_tail - dollar_ptr - 1);
-	if (!env_key)
-	{
-		report_error("expand_env_var", NULL, "strndup failed");
-		mgr->status = -1;
-		return ;
-	}
-	value = mgr->env_table->search(mgr->env_table, env_key);
-	free(env_key);
-	if (value == NULL)
-	{
-		// 環境変数が見つからない場合の処理
-		memmove(dollar_ptr, env_tail, strlen(env_tail) + 1);
-		return ;
-	}
-	*dollar_ptr = '\0';
-	len = strlen(word_list->token->word) + strlen(value) + strlen(env_tail);
-	new_word = calloc(len + 1, sizeof(char));
-	if (!new_word)
-	{
-		report_error("expand_env_var", NULL, "calloc failed");
-		mgr->status = -1;
-		return ;
-	}
-	strcpy(new_word, word_list->token->word);
-	strcat(new_word, value);
-	strcat(new_word, env_tail);
-	free(word_list->token->word);
-	word_list->token->word = new_word;
-}
-
-void	expand_var_loop(t_word *word_list, t_mgr *mgr)
-{
-	t_word	*word_to_expand;
-	t_word	*next;
-
-	word_to_expand = word_list;
-	if (word_list->token->type == TK_WORD)
-	{
-		expand_env_var(word_to_expand, mgr);
-		return ;
-	}
-	if (word_list->token->type == TK_DQUOTE)
-	{
-		while (strchr(word_to_expand->token->word, '$'))
-		{
-			expand_env_var(word_to_expand, mgr);
-		}
-	}
-}
-
 char	**ft_split_multidelim(char *str, const char *delimiters)
 {
 	char	**splitted;
@@ -75,7 +9,6 @@ char	**ft_split_multidelim(char *str, const char *delimiters)
 
 	i = 0;
 	// ft_splitで複数の区切り文字を指定することができないため、区切り文字をスペースに変換する。
-	// （ft_splitの改良版をちゃんと作成した際には削除する）
 	while (str[i])
 	{
 		if (strchr(delimiters, str[i]))
@@ -101,6 +34,7 @@ void	free_2d_array(void **array)
 	free(array);
 }
 
+// 改装中
 void	word_splitting(t_word *word_list)
 {
 	char	**splitted;
@@ -121,8 +55,6 @@ void	word_splitting(t_word *word_list)
 	next_word = word_list->next;
 	next_token = word_list->token->next;
 	word_list->token->word = splitted[0];
-	word_list->token->next = NULL; // 必要？
-	word_list->next = NULL;        // 必要？
 	i = 1;
 	while (splitted[i])
 	{
@@ -149,83 +81,115 @@ void	word_splitting(t_word *word_list)
 	word_list->token->next = next_token;
 }
 
-/*
-
-TMP_FUNC_NAME は関数名を一時的につけたもので、適切な関数名に変更する必要がある
-
-	`aaa"ccccc"bbb` という文字列がある場合、`aaa` と `ccccc` と `bbb` にtokenとして
-	分割されて、それぞれwordtoken, quotedtoken, wordtokenとなる。
-	トークンとして分割された状態で展開の処理を経て、最終的には
-	統合して `aaaccccbbb` という文字列になりたいので、くっつける関数が必要になる
-	*/
-
-bool	should_merge(t_word *word)
+char	*extract_env_key(char *env_head, char **env_tail)
 {
-	if (!word || !word->next)
-		return (false);
-	if (!is_word_or_quoted_token(word->token)
-		|| !is_word_or_quoted_token(word->next->token))
-		return (false);
-	return (word->token->next == word->next->token);
+	char	*env_key;
+
+	if (!env_head || !*env_head)
+		return (NULL);
+	*env_tail = env_head;
+	while (**env_tail && !strchr(IFS, **env_tail))
+		(*env_tail)++;
+	env_key = strndup(env_head, *env_tail - env_head);
+	if (!env_key)
+	{
+		report_error("expand_env", NULL, "strndup failed");
+		return (NULL);
+	}
+	return (env_key);
 }
 
-void	merge_adjacent_words(t_word *word)
+void	expand_env(t_token *word_token, char **cur_ptr, t_hash_table *env_table)
 {
-	t_word	*next_word;
-	t_word	*next_next_word;
-	t_token	*next_next_token;
-	char	*new_word;
+	char	*dollar_ptr;
+	char	*env_key;
+	char	*env_tail;
+	char	*value;
 	size_t	len;
+	char	*new_word;
 
-	next_word = word->next;
-	len = strlen(word->token->word) + strlen(next_word->token->word) + 1;
-	new_word = calloc(len, sizeof(char));
+	dollar_ptr = strchr(*cur_ptr, '$'); // TODO: ft_strchrに変更
+	if (!dollar_ptr || *(dollar_ptr + 1) == '\0')
+	{
+		*cur_ptr = NULL;
+		return ;
+	}
+	if (*(dollar_ptr + 1) == '$')
+	{
+		// $$の処理 -> 本来はプロセスIDに置き換えるがminishellでは対象外
+		*cur_ptr = dollar_ptr + 2;
+		return ;
+	}
+	if (*(dollar_ptr + 1) == '?')
+	{
+		// $?の処理 -> 直前のコマンドの終了ステータスに置き換える
+		*cur_ptr = dollar_ptr + 2;
+		return ;
+	}
+	env_key = extract_env_key(dollar_ptr + 1, &env_tail);
+	if (!env_key)
+	{
+		*cur_ptr = NULL; //  TODO: error handling
+		return ;
+	}
+	value = env_table->search(env_table, env_key);
+	free(env_key);
+	if (value == NULL)
+	{
+		// 環境変数が見つからない場合の処理
+		memmove(dollar_ptr, env_tail, strlen(env_tail) + 1);
+		// TODO: ft_memmoveに変更
+		*cur_ptr = dollar_ptr;
+		// 次の展開処理を続けるためにcur_ptrを更新
+		return ;
+	}
+	*dollar_ptr = '\0';
+	// env_tailにはenv_keyが終了した位置（IFSまたは文字列の終端）が入っている
+	len = strlen(word_token->word) + strlen(value) + strlen(env_tail);
+	// TODO: ft_strlenに変更
+	new_word = calloc(len + 1, sizeof(char));
 	if (!new_word)
 	{
+		report_error("expand_env", NULL, "calloc failed");
 		// TODO: error handling
-		assert_error("calloc failed", "merge_adjacent_words");
+		*cur_ptr = NULL;
+		return ;
 	}
-	strcpy(new_word, word->token->word);
-	strcat(new_word, next_word->token->word);
-	free(word->token->word);
-	word->token->word = new_word;
-	next_next_word = next_word->next;
-	next_next_token = next_next_word->token;
-	free(next_word->token->word);
-	free(next_word->token);
-	free(next_word);
-	word->next = next_next_word;
-	word->token->next = next_next_token;
+	// TODO: この辺も、のちにft_strcpy, ft_strcatに変更が必要
+	strcpy(new_word, word_token->word);
+	strcat(new_word, value);
+	strcat(new_word, env_tail);
+	free(word_token->word);
+	word_token->word = new_word;
+	*cur_ptr = dollar_ptr + strlen(value); // ?
 }
 
-void	merge_words(t_word *word_list)
+void	expand_word_token(t_token *word_token, t_hash_table *env_table)
 {
-	t_word	*word_to_check;
+	char	*current_ptr;
 
-	word_to_check = word_list;
-	while (word_to_check && word_to_check->next)
+	if (word_token->type != TK_WORD && word_token->type != TK_DQUOTE)
+		return ;
+	current_ptr = word_token->word;
+	while (current_ptr && *current_ptr)
 	{
-		if (should_merge(word_to_check))
-			merge_adjacent_words(word_to_check);
-		else
-			word_to_check = word_to_check->next;
+		expand_env(word_token, &current_ptr, env_table);
 	}
 }
 
 void	expand_word_list(t_word *word_list, t_hash_table *env_table)
 {
 	t_word	*word_to_expand;
-	t_word	*next;
+	t_word	*next_word;
 
 	word_to_expand = word_list;
 	while (word_to_expand)
 	{
-		next = word_to_expand->next;
-		// expand_env_loop(word_to_expand, env_table); これから作る予定？
+		next_word = word_to_expand->next;
+		expand_word_token(word_to_expand, env_table);
 		word_splitting(word_to_expand);
-		word_to_expand = next;
+		word_to_expand = next_word;
 	}
-	merge_words(word_list);
 }
 
 void	expand_redir_list(t_redir *redir_list, t_hash_table *env_table)
@@ -234,8 +198,23 @@ void	expand_redir_list(t_redir *redir_list, t_hash_table *env_table)
 	{
 		if (redir_list->redir_type == TK_HEREDOC)
 		{
-			merge_words(redir_list->word_list);
-			// TODO : here documentの処理を追加
+			/*
+				NOTE: bashにおけるheredocの挙動について
+					$ cat <<EOF        <- heredocは変数展開される
+					$ cat <<'EOF'      <- heredocは変数展開されない
+					$ cat <<"EOF"      <- heredocは変数展開されない
+					$ cat <<$EOF       <- デリミタは変数展開されず、heredocは変数展開される
+					$ cat <<"$EOF"     <- デリミタは変数展開されず、heredocは変数展開されない
+
+				いずれの場合も、heredocの中身がword splittingされることはない
+
+				デリミタがquoteをまたいで、複数のtokenにまたがる場合は
+				heredoc実行の前にデリミタがmergeされていなければならない
+
+				ただしデリミタがquoteを含むかどうかは、heredocの変数展開に必要な情報なので
+				mergeされる際には、quoteが含まれるかどうかを保持しておく必要がある
+				TODO: あとで実装
+			*/
 		}
 		else
 		{
@@ -245,26 +224,77 @@ void	expand_redir_list(t_redir *redir_list, t_hash_table *env_table)
 	}
 }
 
-void	expand_exec(t_execcmd *ecmd, t_hash_table *env_table)
-{
-	expand_word_list(ecmd->word_list, env_table);
-	expand_redir_list(ecmd->redir_list, env_table);
-}
-
-void	expand_pipe(t_pipecmd *pcmd, t_hash_table *env_table)
-{
-	run_expansion(pcmd->left, env_table);
-	run_expansion(pcmd->right, env_table);
-}
-
 void	run_expansion(t_cmd *cmd, t_hash_table *env_table)
 {
+	t_execcmd	*ecmd;
+	t_pipecmd	*pcmd;
+
 	if (!cmd || cmd->type == NONE || !env_table)
 		return ;
 	else if (cmd->type == EXEC)
-		expand_exec((t_execcmd *)cmd, env_table);
+	{
+		ecmd = (t_execcmd *)cmd;
+		expand_word_list(ecmd->word_list, env_table);
+		expand_redir_list(ecmd->redir_list, env_table);
+	}
 	else if (cmd->type == PIPE)
-		expand_pipe((t_pipecmd *)cmd, env_table);
+	{
+		pcmd = (t_pipecmd *)cmd;
+		run_expansion(pcmd->left, env_table);
+		run_expansion(pcmd->right, env_table);
+	}
 	else
 		exit(EXIT_FAILURE);
 }
+/*
+note: exapansionの対象となるのは以下の構造体におけるword_listとredir_listである
+
+typedef struct s_token
+{
+	enum e_token_type	type;
+	char				*word;
+	struct s_token		*next;
+}						t_token;
+
+typedef struct s_word
+{
+	struct s_token		*token;
+	struct s_word		*next;
+}						t_word;
+
+typedef struct s_redir
+{
+	enum e_token_type	redir_type;
+	int					fd;
+	int					backup_fd;
+	struct s_word		*word_list;
+	struct s_redir		*next;
+}						t_redir;
+
+typedef enum e_cmd_type
+{
+	NONE,
+	EXEC,
+	PIPE
+}						t_cmd_type;
+
+typedef struct s_cmd
+{
+	enum e_cmd_type		type;
+}						t_cmd;
+
+typedef struct s_execcmd
+{
+	enum e_cmd_type		type;
+	struct s_word		*word_list;
+	struct s_redir		*redir_list;
+	char				*eof_word;
+}						t_execcmd;
+
+typedef struct s_pipecmd
+{
+	enum e_cmd_type		type;
+	struct s_cmd		*left;
+	struct s_cmd		*right;
+}						t_pipecmd;
+ */
