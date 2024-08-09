604d72148907b0c04c073ab15a5f26d76e58a74a
skitheom <SahoKitahara@my.uopeople.edu>
2024-08-09 16:12:05 +0900

    right_cmd->redir_listのleakとり

--- src/ast.c
+++ src/ast.c
@@ -32,8 +32,8 @@ void	free_redir(t_redir *redir)
 // ダブルフリー防止用
 void	free_right_cmd(t_cmd *cmd)
 {
-	t_execcmd *ecmd;
-	t_word	*next;
+	t_execcmd	*ecmd;
+	t_word		*next;
 
 	ecmd = (t_execcmd *)cmd;
 	if (!cmd)
@@ -47,7 +47,7 @@ void	free_right_cmd(t_cmd *cmd)
 			ecmd->word_list = next;
 		}
 		// free(ecmd->word_list->token);
-		// free_redir(((t_execcmd *)cmd)->redir_list);
+		free_redir(ecmd->redir_list);
 	}
 	else if (cmd->type == PIPE)
 	{
@@ -62,7 +62,7 @@ void	free_right_cmd(t_cmd *cmd)
 /* free_cmd - t_cmd構造体およびその派生構造体を解放する関数 */
 void	free_cmd(t_cmd *cmd)
 {
-	t_execcmd *ecmd;
+	t_execcmd	*ecmd;
 
 	ecmd = (t_execcmd *)cmd;
 	if (!cmd)
@@ -75,7 +75,8 @@ void	free_cmd(t_cmd *cmd)
 	}
 	else if (cmd->type == PIPE)
 	{
-		//左と右両方やると, 二重freeになる箇所あり. たぶんleftのtokenとかword_listで, rightの内容も含んでる部分だと思う. 個別対応必須.
+		// 左と右両方やると, 二重freeになる箇所あり. たぶんleftのtokenとかword_listで,
+		// rightの内容も含んでる部分だと思う. 個別対応必須.
 		free_cmd(((t_pipecmd *)cmd)->left);
 		// free_cmd(((t_pipecmd *)cmd)->right);
 		free_right_cmd(((t_pipecmd *)cmd)->right);
