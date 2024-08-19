b7adc7d9e22d25a3e30407cc9972db901a5278bf
skitheom <SahoKitahara@my.uopeople.edu>
2024-08-20 03:23:04 +0900

    utils.h 整理

--- include/utils.h
+++ include/utils.h
@@ -2,10 +2,15 @@
 #ifndef UTILS_H
 # define UTILS_H
 
+# include <stddef.h>
+
+/* utils.c */
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
+
+/* builtin.c ? */
 size_t	ft_strlen(const char *str);
 char	*ft_strndup(char *src, long len);
 char	*ft_strdup(char *src);
