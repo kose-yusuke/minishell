1fc13cd8df968a8ba9fc89bcea116bc7793c5cee
skitheom <SahoKitahara@my.uopeople.edu>
2024-10-01 04:37:57 +0900

    空文字列のargvの処理を修正

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:48:26 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/10/01 03:35:05 by sakitaha         ###   ########.fr       */
+/*   Updated: 2024/10/01 04:37:28 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 static bool	is_valid_arg(t_arg *arg)
 {
-	if (!arg || !arg->token || !arg->token->word || !arg->token->word[0])
+	if (!arg || !arg->token || !arg->token->word)
 		return (false);
 	if (!is_word_or_quoted_token(arg->token))
 		return (false);
@@ -27,15 +27,15 @@ static bool	is_valid_arg(t_arg *arg)
 static size_t	count_valid_args(t_arg *arg_list)
 {
 	size_t	count;
-	t_arg	*tmp_arg;
+	t_arg	*arg;
 
 	count = 0;
-	tmp_arg = arg_list;
-	while (tmp_arg)
+	arg = arg_list;
+	while (arg)
 	{
-		if (is_valid_arg(tmp_arg))
+		if (is_valid_arg(arg))
 			count++;
-		tmp_arg = tmp_arg->next;
+		arg = arg->next;
 	}
 	return (count);
 }
@@ -43,22 +43,23 @@ static size_t	count_valid_args(t_arg *arg_list)
 char	**convert_list_to_array(t_arg *arg_list)
 {
 	char	**argv;
-	t_arg	*tmp_arg;
+	t_arg	*arg;
 	size_t	len;
 	size_t	i;
 
 	if (!arg_list)
 		return (NULL);
-	tmp_arg = arg_list;
-	len = count_valid_args(arg_list);
+	arg = arg_list;
+	while (arg && (!arg->token || !arg->token->word || !arg->token->word[0]))
+		arg = arg->next;
+	len = count_valid_args(arg);
 	argv = (char **)xmalloc(sizeof(char *) * (len + 1));
-	tmp_arg = arg_list;
 	i = 0;
-	while (tmp_arg)
+	while (arg)
 	{
-		if (is_valid_arg(tmp_arg))
-			argv[i++] = ft_strdup(tmp_arg->token->word);
-		tmp_arg = tmp_arg->next;
+		if (is_valid_arg(arg))
+			argv[i++] = ft_strdup(arg->token->word);
+		arg = arg->next;
 	}
 	argv[i] = NULL;
 	return (argv);
